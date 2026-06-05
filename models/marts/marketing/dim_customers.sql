4457f9db5ab0b3154cc399209b83779c73e38fb8
Jose <jose.marquez.ipp.cl@gmail.com>
2026-06-05 09:37:21 +0000

    Agregue la tabla fact_ordes, cambie dim_customers y agregue el archivo source por origen

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -8,7 +8,7 @@ with customers as (
 orders as (
 
     select *
-    from {{ ref('stg_jaffle_shop__orders') }}
+    from {{ ref('fct_orders') }}
 
 ),
 
@@ -16,13 +16,11 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
     from orders
-
     group by 1
 
 ),
@@ -36,12 +34,11 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        lifetime_value
     from customers
 
     left join customer_orders using (customer_id)
-
 )
 
 select * from final
