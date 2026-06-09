345d19be9907e8df9ec22bb05e6d9d09b2fb6d78
Jose <jose.marquez.ipp.cl@gmail.com>
2026-06-09 09:26:03 +0000

    Agrego prueba de datos: únicos, valores aceptado y no nulos

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,19 @@
+with 
+
+order_table AS (
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status as order_status
+    from {{ source('jaffle_shop', 'orders') }}
+
+)
+
+
 select
-    id as order_id,
-    user_id as customer_id,
+    order_id,
+    customer_id,
     order_date,
-    status as order_status
-from {{ source('jaffle_shop', 'orders') }}
+    order_status
+from order_table
