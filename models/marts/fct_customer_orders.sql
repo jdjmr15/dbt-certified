b4917336c177e0674fc035dea98daa13952ddabf
Jose <jose.marquez.ipp.cl@gmail.com>
2026-06-16 09:24:06 +0000

    Finalizo la recfatorizacion del codigo legacy

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,74 +1,22 @@
 
 with
-    --staging
-
-    customers as (
-        select *
-        from {{ ref('stg_customers') }}
-    ),
-
-    orders as (
-        select *
-        from {{ ref('int_orders') }}
-    ),
-
-    -- Logical CTEs
-    customer_order_history as (
-        select
-            customers.customer_id,
-            full_name,
-            surname,
-            givenname,
-            min(order_date) as first_order_date,
-            min(valid_order_date) as first_non_returned_order_date,
-            max(valid_order_date) as most_recent_non_returned_order_date,
-            coalesce(max(user_order_seq), 0) as order_count,
-            coalesce(
-                count(case when valid_order_date is not null then 1 end), 0
-            ) as non_returned_order_count,
-            sum(
-                case
-                    when valid_order_date is not null
-                    then order_value_dollars
-                    else 0
-                end
-            ) as total_lifetime_value,
-            sum(
-                case
-                    when valid_order_date is not null
-                    then order_value_dollars
-                    else 0
-                end
-            ) / nullif(
-                count(
-                    case when valid_order_date is not null then 1 end
-                ),
-                0
-            ) as avg_non_returned_order_value,
-            array_agg(distinct orders.order_id) as order_ids
-
-        from orders
-        join customers on orders.customer_id = customers.customer_id
-        group by customers.customer_id, full_name, surname, givenname
-
-    ),
-
-    -- Final CTE
+    customer_orders as (
+        select * from {{ ref('int_customers') }}
+    ), 
+    
     final as (
         select
-            orders.order_id,
-            orders.customer_id,
-            customers.surname,
-            customers.givenname,
-            first_order_date,
-            order_count,
-            total_lifetime_value,
-            orders.order_value_dollars,
-            orders.order_status as order_status,
-            orders.payment_status
-        from orders
-        join customers on orders.customer_id = customers.customer_id
-        join customer_order_history on orders.customer_id = customer_order_history.customer_id
+            order_id,
+            customer_id,
+            surname,
+            givenname,
+            customer_first_order_date as first_order_date,
+            customer_order_count as order_count,
+            customer_total_lifetime_value as total_lifetime_value,
+            order_value_dollars,
+            order_status as order_status,
+            payment_status
+        from customer_orders
         
     )
 
---
+++ models/marts/intermediate/int_customers.sql
@@ -0,0 +1,38 @@
+    customers as (
+        select *
+        from {{ ref('stg_customers') }}
+    ),
+
+    orders as (
+        select *
+        from {{ ref('int_orders') }}
+    ),
+
+    customer_orders as (
+        select
+            orders.*,
+            customers.full_name,
+            customers.surname,
+            customers.givenname,
+
+            min(order_date) over (partition by customers.customer_id) as customer_first_order_date,
+            min(valid_order_date) over (partition by customers.customer_id) as customer_first_non_returned_order_date,
+            max(valid_order_date) over (partition by customers.customer_id) as customer_most_recent_non_returned_order_date,
+            count(*)  over (partition by customers.customer_id) as customer_order_count,
+            sum(nvl2(valid_order_date,1,0)) over (partition by customers.customer_id) as customer_non_returned_order_count,
+            sum(nvl2(valid_order_date,order_value_dollars,0)) over (partition by customers.customer_id) as customer_total_lifetime_value,
+            array_agg(distinct orders.order_id) over (partition by customers.customer_id) as customer_order_ids
+
+
+        from orders
+        inner join customers using (customer_id)
+    ),
+
+    customer_average_order_value as (
+        select
+            *,
+            (customer_total_lifetime_value / customer_non_returned_order_count) as customer_avg_non_returned_order_value
+        from customer_orders
+    )
+
+select * from customer_average_order_value
