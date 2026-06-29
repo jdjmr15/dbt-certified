02f970be7d2c0537fa1d1fdfe095bd1020413567
Jose <jose.marquez.ipp.cl@gmail.com>
2026-06-29 12:37:45 +0000

    Diccionario en jinja

---
+++ analyses/jinja_syntax/dict_jinja.sql
@@ -0,0 +1,9 @@
+{% set 
+    websters_dict = {
+        'word' : 'data',
+        'speech_part' : 'noun',
+        'definition' : 'if you know you know'
+    } 
+-%}
+
+{{ websters_dict['word'] }} ({{ websters_dict['speech_part'] }}): define as "{{ websters_dict['definition'] }}"
